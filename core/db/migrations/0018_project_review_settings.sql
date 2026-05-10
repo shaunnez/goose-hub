bb066c9ef5bc064e08f544c2307fcc7590c76927
Shaun Nesbitt <uksn@me.com>
2026-05-11 10:05:58 +1200

    Merge main: rename migration to 0019

--- core/db/migrations/0018_project_review_settings.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE `project_review_settings` (
-	`project_id` text PRIMARY KEY NOT NULL,
-	`reviewer_slots` text,
-	`updated_at` text DEFAULT (strftime('%Y-%m-%dT%H:%M:%SZ', 'now')) NOT NULL,
-	`updated_by` text
-);
