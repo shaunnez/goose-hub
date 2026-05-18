3bc322fb96f4a4229a9d3098b52eefeb808f0b2e
Shaun Nesbitt <shaun@smartpay.co.nz>
2026-05-18 19:37:48 +1200

    minor

---
+++ core/db/migrations/0030_wild_slyde.sql
@@ -0,0 +1,6 @@
+DROP TABLE `project_model_settings`;--> statement-breakpoint
+ALTER TABLE `project_settings` ADD `qa_e2e_mode` text;--> statement-breakpoint
+ALTER TABLE `project_settings` ADD `playwright_repro_enabled` integer;--> statement-breakpoint
+ALTER TABLE `project_settings` ADD `evidence_post_enabled` integer;--> statement-breakpoint
+ALTER TABLE `project_skill_settings` ADD `model_tier` text;--> statement-breakpoint
+ALTER TABLE `project_skill_settings` ADD `model_provider` text;
